a4fec089fbb0fd5df8b633beea5f7740ca0f9e3a
Pablets <godoypablom@gmail.com>
2021-03-21 12:04:32 -0300

    added "important" default funtion

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ let notes = [
 ]
 
 app.get('/', (req, res) => {
-  res.json(`Hello World!!!`)
+  res.send(`<h1>Hello World!!!</h1>`)
 })
 
 app.get('/api/notes', (req, res) => {
@@ -49,19 +49,50 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.post('/api/notes', (req, res) => {
-  const note = req.body
-  console.log(note)
-  const ids = notes.map(note => note.id)
-  const maxId = Math.max(...ids)
-  const newNote = {
-    id: maxId + 1,
-    content: note.content,
-    important: typeof note.important !== undefined ? note.important : false,
-    date: new Date().toISOString(),
+// app.post('/api/notes', (req, res) => {
+//   const note = req.body
+//   console.log(note)
+//   console.log(typeof note.important)
+//   const ids = notes.map(note => note.id)
+//   const maxId = Math.max(...ids)
+//   const newNote = {
+//     id: maxId + 1,
+//     content: note.content,
+//     important: note.important || false,
+//     date: new Date().toISOString(),
+//   }
+//   notes = [...notes, newNote]
+//   res.status(201).json(note)
+// })
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    })
   }
-  notes = [...notes, newNote]
-  res.status(201).json(note)
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  console.log(notes)
+
+  response.json(note)
 })
 
 const PORT = process.env.PORT || 3001
